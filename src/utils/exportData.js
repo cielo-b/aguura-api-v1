e1c046242832d9644ac6c1fcab366664f9ff5d6d
dabagirevalens <vdabagire@gmail.com>
2024-10-02 22:23:01 -0400

    [feat] update fonts location

--- src/utils/exportData.js
+++ src/utils/exportData.js
@@ -20,13 +20,13 @@ const reportsDirectory = path.join(__dirname, "../../public/reports");
 
 // Fonts
 
-// const JainiPurva = '/usr/share/fonts/JainiPurva-Regular.ttf';
-// const PoppinsRegular = '/usr/share/fonts/Poppins-Regular.ttf';
-// const PoppinsBold = '/usr/share/fonts/Poppins-Bold.ttf';
+const JainiPurva = '/usr/share/fonts/aguurafonts/JainiPurva-Regular.ttf';
+const PoppinsRegular = '/usr/share/fonts/aguurafonts/Poppins-Regular.ttf';
+const PoppinsBold = '/usr/share/fonts/aguurafonts/Poppins-Bold.ttf';
 
-const JainiPurva = "c:/fonts/Jaini_Purva/JainiPurva-Regular.ttf";
-const PoppinsRegular = "c:/fonts/Poppins/Poppins-Regular.ttf";
-const PoppinsBold = "c:/fonts/Poppins/Poppins-Bold.ttf";
+// const JainiPurva = "c:/fonts/Jaini_Purva/JainiPurva-Regular.ttf";
+// const PoppinsRegular = "c:/fonts/Poppins/Poppins-Regular.ttf";
+// const PoppinsBold = "c:/fonts/Poppins/Poppins-Bold.ttf";
 
 const exportData = async (stock, activeDay) => {
   const type = stock.type;
